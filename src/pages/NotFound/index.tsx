845d8afeed9c3d928ec5479354a6bc9f25cddcf5
Aleksey Popov <alekseypopov.dev@gmail.com>
2022-10-28 20:02:56 +0700

    feat: add notfound

---
+++ src/pages/NotFound/index.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from 'react';
+
+const NotFound: FC = () => {
+  return (
+    <section>
+      <h1>Go back, stranger</h1>
+    </section>
+  );
+};
+
+export default NotFound;
