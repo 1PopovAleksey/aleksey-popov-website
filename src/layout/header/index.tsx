6bbc686313d4e42fa19be5d032e1d6e09a6842c4
Aleksey Popov <alekseypopov.dev@gmail.com>
2022-10-28 20:02:05 +0700

    feat: update header

--- src/layout/header/index.tsx
+++ src/layout/header/index.tsx
@@ -1,11 +1,19 @@
 import React, { FC } from 'react';
 
+import style from './header.module.scss';
+
+import { Logo } from './icon';
+
 const Header: FC = () => {
   return (
-    <header>
-      
+    <header className={style.header}>
+      <div className={style.container}>
+        <div className={style.offset}>
+          <Logo />
+        </div>
+      </div>
     </header>
   );
-}
+};
 
 export default Header;
