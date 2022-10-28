6a8e20bfd43c8208ac1a3b9d158f277db9f2cc30
Aleksey Popov <alekseypopov.dev@gmail.com>
2022-10-28 20:02:16 +0700

    feat: update main

--- src/layout/main/index.tsx
+++ src/layout/main/index.tsx
@@ -1,11 +1,15 @@
 import React, { FC } from 'react';
 import { Routes, Route } from 'react-router-dom';
 
+import Home from '../../pages/Home';
+import NotFound from '../../pages/NotFound';
+
 const Main: FC = () => {
   return (
     <main>
       <Routes>
-        <Route path={'/'} element={''} />
+        <Route path={'/'} element={<Home />} />
+        <Route path={'*'} element={<NotFound />} />
       </Routes>
     </main>
   );
