3b15fcb725b3d3bc59b72c9c2dddcb851d81bc06
Aleksey Popov <alekseypopov.dev@gmail.com>
2022-10-29 20:27:45 +0700

    feat: update Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,15 +1,43 @@
 import React, { FC } from 'react';
 
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination } from 'swiper';
+
+import 'swiper/css';
+import 'swiper/css/pagination';
+
 import style from './home.module.scss';
 
 const Home: FC = () => {
   return (
     <section className={style.slider}>
       <div className={style.container}>
-        <div className={style.offset}>
-          <h1>Hi, I’m Aleksey Popov</h1>
-          <h1>And I’m Frontend developer</h1>
-        </div>
+        <Swiper
+          direction={'vertical'}
+          pagination={{
+            clickable: true,
+          }}
+          modules={[Pagination]}
+          className={style.mySwiper}>
+          <SwiperSlide>
+            <div className={style.offset}>
+              <h1>Hi, I’m Aleksey Popov</h1>
+              <h1>And I’m Frontend developer</h1>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className={style.offset}>
+              <h1>Hi, I’m Aleksey Popov</h1>
+              <h1>And I’m Frontend developer</h1>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className={style.offset}>
+              <h1>Hi, I’m Aleksey Popov</h1>
+              <h1>And I’m Frontend developer</h1>
+            </div>
+          </SwiperSlide>
+        </Swiper>
       </div>
     </section>
   );
