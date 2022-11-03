ea29b1d7a9028e4bf9d0a4edc99e9b49e832a4b2
Aleksey Popov <alekseypopov.dev@gmail.com>
2022-11-04 05:11:38 +0700

    feat: update home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -5,6 +5,10 @@ import { Mousewheel, Keyboard } from 'swiper';
 
 import 'swiper/css';
 
+import me from '../../assets/img/me.png';
+
+import { Github, Gitlab, Telegram, Vk } from './icons';
+
 import style from './home.module.scss';
 
 const Home: FC = () => {
@@ -21,20 +25,41 @@ const Home: FC = () => {
           className={style.mySwiper}>
           <SwiperSlide>
             <div className={style.offset}>
-              <h1 className={style.title}>
-                Lorem Ipsum is <span className={style.green}>simply</span> dummy text of the
-                printing.
-              </h1>
-              <p className={style.paragraph}>
-                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,{' '}
-                <span className={style.green}>when an unknown</span> printer took a galley of type
-                and scrambled it to make a type specimen book.
-              </p>
-              <p className={style.paragraph}>
-                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,{' '}
-                <span className={style.green}>when an unknown</span> printer took a galley of type
-                and scrambled it to make a type specimen book.
-              </p>
+              <div className={style.content}>
+                <div className={style.content_text}>
+                  <div>
+                    <h1 className={style.title}>
+                      Lorem Ipsum is <span className={style.green}>simply</span> dummy text of the
+                      printing.
+                    </h1>
+                    <p className={style.paragraph}>
+                      Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,{' '}
+                      <span className={style.green}>when an unknown</span> printer took a galley of
+                      type and scrambled it to make a type specimen book.
+                    </p>
+                    <p className={style.paragraph}>
+                      Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,{' '}
+                      <span className={style.green}>when an unknown</span> printer took a galley of
+                      type and scrambled it to make a type specimen book.
+                    </p>
+                  </div>
+                  <div className={style.icons}>
+                    <a href="https://t.me/AlekseyPopovDev">
+                      <Telegram />
+                    </a>
+                    <a href="https://vk.com/1popovaleksey">
+                      <Vk />
+                    </a>
+                    <a href="https://github.com/1PopovAleksey">
+                      <Github />
+                    </a>
+                    <a href="https://gitlab.com/alekseypopov.dev">
+                      <Gitlab />
+                    </a>
+                  </div>
+                </div>
+                <img className={style.image} src={me} alt="It's me" />
+              </div>
             </div>
           </SwiperSlide>
           <SwiperSlide>
