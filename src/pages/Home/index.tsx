ab47df26a7f6a4a07e6808cd0d2c9ea0cc1bd888
Aleksey Popov <alekseypopov.dev@gmail.com>
2022-11-03 22:09:38 +0700

    feat: update Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,9 @@
 import React, { FC } from 'react';
 
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Pagination } from 'swiper';
+import { Mousewheel, Keyboard } from 'swiper';
 
 import 'swiper/css';
-import 'swiper/css/pagination';
 
 import style from './home.module.scss';
 
@@ -14,15 +13,28 @@ const Home: FC = () => {
       <div className={style.container}>
         <Swiper
           direction={'vertical'}
-          pagination={{
-            clickable: true,
+          keyboard={{
+            enabled: true,
           }}
-          modules={[Pagination]}
+          mousewheel={true}
+          modules={[Mousewheel, Keyboard]}
           className={style.mySwiper}>
           <SwiperSlide>
             <div className={style.offset}>
-              <h1>Hi, I’m Aleksey Popov</h1>
-              <h1>And I’m Frontend developer</h1>
+              <h1 className={style.title}>
+                Lorem Ipsum is <span className={style.green}>simply</span> dummy text of the
+                printing.
+              </h1>
+              <p className={style.paragraph}>
+                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,{' '}
+                <span className={style.green}>when an unknown</span> printer took a galley of type
+                and scrambled it to make a type specimen book.
+              </p>
+              <p className={style.paragraph}>
+                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,{' '}
+                <span className={style.green}>when an unknown</span> printer took a galley of type
+                and scrambled it to make a type specimen book.
+              </p>
             </div>
           </SwiperSlide>
           <SwiperSlide>
